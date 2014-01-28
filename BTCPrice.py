69008d4b3295530013fbe76a9cc7f9bb1f95a8df
Adam Walsh <adamwalsh14@gmail.com>
2014-01-28 13:33:48 -0500

    Inital commit of BTCPrice.py geeklet

---
+++ BTCPrice.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#Created on 1/28/2014
+#Maintained @ 'https://github.com/walshie4/Geeklets'
+
+#This script requires the requests library
+#For install instructions go to 'http://docs.python-requests.org/en/latest/user/install/#install'
+#It is awesome and you should have it
+
+from __future__ import print_function
+import requests
+
+__author__='walshie4'
+
+ticker='https://www.bitstamp.net/api/ticker/'
+
+def getCurrentInfo():
+    try:
+        return requests.get(ticker).json()
+    except Error:
+        return {'Error' : 'Error'}
+
+
+if __name__=='__main__':
+    json = getCurrentInfo()
+    if 'Error' in json:
+        print('An Error occured. Check internet connection.')
+    else:
+        print('Current Price:\t$',json['last'])
+        print('24H High:\t$',json['high'])
+        print('24H Low:\t$',json['low'])
+        print('24H Volume:\t',json['volume'])
