c598dc68ae8ae1aad4d8967904daaf40cc1d3769
Adam Walsh <adamwalsh14@gmail.com>
2014-02-03 13:29:15 -0500

    Added information about the percent of packets lost

--- ServerStatus.sh
+++ ServerStatus.sh
@@ -3,14 +3,15 @@
 #Maintained at https://github.com/walshie4/Geeklets
 #Created on 2/2/2014
 #Server list config
-SERVER='www.something.com' #put URL in ''s in this variable 
+SERVER='www.rit.edu' #put URL in ''s in this variable 
 #End config
 
 response=`ping -c 1 $SERVER`
 if [[ -n "$response" ]]; then #if response is non-zero (got a response)
     ip=`echo $response | cut -f2 -d '(' | cut -f1 -d ')'` #get the ip
     time=`echo $response | cut -f4 -d '=' | cut -f1 -d ' '` #get the response time
-    echo "The server at $ip ($SERVER) responded in $time ms."  #print the info
+    lost=`echo $response | cut -f1 -d '%' | tail -c 10 | cut -f2 -d ' '` #get the percent of packets lost
+    echo "The server at $ip ($SERVER) responded in $time ms, and lost $lost% of the packets sent"  #print the info
 else
     echo "Invalid hostname or not connected to the internet" #alert the user of error
 fi
