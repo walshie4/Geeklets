1373d30916cca483ffb7619ec00dcba029496df4
Adam Walsh <adamwalsh14@gmail.com>
2014-02-03 16:14:02 -0500

    Now uses 5 ping average. Fixed an issue with what to do when all packets are lost (no connection)

--- ServerStatus.sh
+++ ServerStatus.sh
@@ -6,12 +6,18 @@
 SERVER='www.rit.edu' #put URL in ''s in this variable 
 #End config
 
-response=`ping -c 1 $SERVER`
+ALL='100.0'
+
+response=`ping -c 5 $SERVER`
 if [[ -n "$response" ]]; then #if response is non-zero (got a response)
     ip=`echo $response | cut -f2 -d '(' | cut -f1 -d ')'` #get the ip
-    time=`echo $response | cut -f4 -d '=' | cut -f1 -d ' '` #get the response time
     lost=`echo $response | cut -f1 -d '%' | tail -c 10 | cut -f2 -d ' '` #get the percent of packets lost
-    echo "The server at $ip ($SERVER) responded in $time ms, and lost $lost% of the packets sent"  #print the info
+    if [ "$lost" = "100.0" ]; then
+        echo "All packets lost when sending to $SERVER"
+        exit 0
+    fi
+    avgtime=`echo $response | cut -f5 -d '/'` #get the average response time
+    echo "The server at $ip ($SERVER) responded in $avgtime ms, and lost $lost% of the packets sent"  #print the info
 else
     echo "Invalid hostname or not connected to the internet" #alert the user of error
 fi
