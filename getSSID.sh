81f513d87528392a6963e2cc7bfc266361f928ef
Rubén Varela <rovr138@gmail.com>
2014-02-04 23:02:12 -0400

    Print your SSID
    Print the SSID you’re currently connected to.

---
+++ getSSID.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Rubén Varela
+# 2013-01-27
+# Really simple script to print the SSID you're connected to.
+
+ssid_without_leading_space=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | sed '/ SSID: /!d; s/^.* SSID: //'`
+echo $ssid_without_leading_space
