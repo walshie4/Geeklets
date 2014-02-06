12b8e5920e07c3576e9e6b8c0462779b699a6b35
Adam Walsh <adamwalsh14@gmail.com>
2014-02-05 23:01:45 -0500

    Added Kbytes / sec for the network device you are monitoring as well.

--- NetworkTraffic.sh
+++ NetworkTraffic.sh
@@ -7,6 +7,14 @@ INTERFACE='en0' #run ifconfig and look for the interface with a status marked 'a
 #End Config
 
 response=`netstat -i -b | grep $INTERFACE`
-in=$((`echo $response | cut -f7 -d ' '` / 1048576)) #divided by number of bytes per megabyte
-out=$((`echo $response | cut -f10 -d ' '` / 1048576))
-echo "TX:$out MBs  RX:$in MBs"
+in=$((`echo $response | cut -f7 -d ' '` / 1024)) #divided by number of bytes per megabyte
+out=$((`echo $response | cut -f10 -d ' '` / 1024))
+in2=$((`echo $response | cut -f7 -d ' '` / 1024)) #divided by number of bytes per megabyte
+out2=$((`echo $response | cut -f10 -d ' '` / 1024))
+inMB=$(($in / 1024))
+outMB=$(($out / 1024))
+echo "TX:$outMB MBs  RX:$inMB MBs"
+sleep 1
+diffIn=$(($in2 - $in))
+diffOut=$(($out2 - $out))
+echo "TX:$diffOut KB/s  RX:$diffIn KB/s"
