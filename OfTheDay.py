0392ed1dfac9896ffd47fa2fa55fc79865f3aafd
Adam Walsh <adamwalsh14@gmail.com>
2014-02-24 21:29:26 -0500

    Finished implementation of number of the day

--- OfTheDay.py
+++ OfTheDay.py
@@ -3,7 +3,7 @@
 #Written by: Adam Walsh (adamwalsh14@gmail.com)
 #Maintained @ https://github.com/walshie4/Geeklets
 
-from __future__ import printfunction
+from __future__ import print_function
 import random
 
 #CONFIG
@@ -13,6 +13,6 @@ NUMBER = True
 numbers = range(-200,201) #all integers [-200, 200]
 
 if NUMBER:
-    print(numbers[random.random()*len(numbers)])
+    print("The number of the day is:", numbers[int(random.random()*len(numbers))])
 else:
     print('No data sources enabled in config')
